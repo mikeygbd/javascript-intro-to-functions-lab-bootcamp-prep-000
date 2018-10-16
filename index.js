2c5ec832f947fe0faaa07cf6642281a3fafffe1d
Michael Soares <mikey.soares@gmail.com>
2018-10-16 17:10:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function logWhisper(string) {
   return string.toLowerCase()
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
+  if (string.toLowerCase() === string){
   return ("I can't hear you!")
 }
-function sayHiToGrandma(string) {
-  if (string.toUpperCase() === string)
+  if (string.toUpperCase() === string){
 return ("YES INDEED!")
+}
 }

e6abf62adbc6eed0a6a3725260caf25759b2a7e4
Michael Soares <mikey.soares@gmail.com>
2018-10-16 16:28:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function logWhisper(string) {
   console.log("hello")
   return string.toLowerCase()
 }
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+  if 'string' = .toLowerCase
+}
